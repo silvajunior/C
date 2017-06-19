98c7c12a5a73d5101caf36f2f2e11252a753e2fd
Everaldo Junior <jr.o2@hotmail.com>
2017-06-19 00:21:01 -0300

    Vetor

---
+++ 08_vetor.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int x[2],i=0;
+	
+		printf("Digite 3 numeros:\n");
+		scanf("%d",&x[0]);
+		scanf("%d",&x[1]);
+		scanf("%d",&x[2]);
+		printf("Os numeros digitados foram: %d, %d, %d. ",x[0],x[1],x[2]);
+}
